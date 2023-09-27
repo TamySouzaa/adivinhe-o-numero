61b485cfd37c2f5585fe99a35c72a55a8d43f7da
Tamiris Souza <tamiris.fsouzaa@outlook.com>
2023-09-27 20:56:28 -0300

    Adivinhe o número

---
+++ script.js
@@ -0,0 +1,37 @@
+var numberToFind = 0;
+var attemps = 0;
+
+function refresh() {
+
+    numberToFind = parseInt(Math.random() * 100);
+
+    console.log(numberToFind)
+}
+
+function verifyNumber() {
+
+    var bet = document.getElementById('bet').value;
+
+    if (bet > 100 || bet < 1) {
+
+        alert('Aposta inválida');
+        return;
+
+    }
+
+    if (bet > numberToFind)
+    {
+        attemps++;
+        alert('🚨 o número é para ser encontrador é MENOR');
+    }
+    else if (bet < numberToFind) {
+        alert('🚨 O número é para ser encontrador é MAIOR');
+    }
+    else
+    {
+      alert('✅ Parabéns, você conseguiu acertar após '+attemps+' tentativas!' );
+    }
+
+}
+
+refresh();
